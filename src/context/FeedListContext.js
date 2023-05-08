7d9cb2dcb2cbf0c6ee71ba69fc03304fc7c87efb
Allisson Araujo <93150352+allissonaraujo@users.noreply.github.com>
2023-05-08 17:11:05 -0300

    Primeiro Commit

---
+++ src/context/FeedListContext.js
@@ -0,0 +1,98 @@
+import createDataContext from './createDataContext';
+
+const feedListReducer = (state, action) => {
+    let newState = [];
+    switch (action.type) {
+        case 'add_feed':
+            newState = [
+                ...state,
+                {
+                    titulo: action.payload.titulo,
+                    urlFeed: action.payload.urlFeed,
+                }
+            ]
+            return newState;
+
+        case 'delete_feed':
+            newState = [
+                ...state.filter((feed) => feed.titulo !== action.payload.titulo)
+            ]
+            return newState;
+
+        case 'restore_state':
+            console.log('implementar');
+            return state;
+        case 'delete_all':
+            console.log('implementar');
+            return state;
+        default:
+            return state;
+    }
+};
+
+const addFeed = dispatch => {
+    return (titulo, urlFeed, callback) => {
+       dispatch({ type: 'add_feed', payload: { titulo, urlFeed } });
+
+       if(callback){
+           callback();
+       }
+
+    };
+};
+
+const deleteFeed = dispatch => {
+    return (titulo) => {
+        dispatch({ type: 'delete_feed', payload: { titulo } });
+    };
+};
+
+const restoreState = dispatch => async () => {
+    return () => {
+        console.log('implementar');
+    }
+}
+
+const deleteAll = dispatch => {
+    return () => {
+        console.log('implementar');
+    }
+}
+
+const rssFeeds = [
+    {
+        titulo: 'G1 - Todas as notícias',
+        urlFeed: 'https://g1.globo.com/rss/g1/',
+        descricao: '',
+        urlSite: '',
+        urlImagem: ''
+    },
+    {
+        titulo: 'G1 - Brasil',
+        urlFeed: 'https://g1.globo.com/rss/g1/brasil/',
+        descricao: '',
+        urlSite: '',
+        urlImagem: ''
+    },
+    {
+        titulo: 'G1 - Tecnologia e Games',
+        urlFeed: 'https://g1.globo.com/rss/g1/tecnologia/',
+        descricao: '',
+        urlSite: '',
+        urlImagem: ''
+    },
+    {
+        titulo: 'Jovem Nerd',
+        urlFeed: 'http://jovemnerd.com.br/rss',
+        descricao: '',
+        urlSite: '',
+        urlImagem: ''
+    }
+    
+];
+
+export const { Context, Provider } = createDataContext(
+    feedListReducer,
+    { addFeed, deleteFeed, restoreState, deleteAll },
+    rssFeeds
+);
---
+++ src/screens/ShowFeedScreen.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import { View, Text, StyleSheet, Button, Image, Linking } from 'react-native';
+import { FlatList, TouchableOpacity } from 'react-native-gesture-handler';
+import { Context as FeedListContext } from '../context/FeedListContext'
+import { Context as FeedContext } from '../context/FeedContext'
+import { useContext, useEffect } from 'react';
+
+
+const ShowFeedScreen = ({ route, navigation }) => {
+    const feedListContext = useContext(FeedListContext);
+    const feedID = route.params.id;
+    const feed = feedListContext.state.find((feed) => feed.urlFeed === feedID);
+    const { state, fetchItems } = useContext(FeedContext);
+
+    useEffect(() => {
+        fetchItems(feed.urlFeed);  
+    })
+
+    const abrirLink = (link) => {
+        Linking.openURL(link);
+    }
+
+    return (
+        <>
+            <FlatList
+                data={state}
+                keyExtractor={(noticia) => noticia.link}
+                renderItem={({ item }) => {
+                    //atualmente só exibe o título, faça com que apareça data de publicação, descrição (pode cortar em 100 ou 200 caracteres para não ficar muito grande), e imagem (caso tenha)
+                    //ao clicar em uma notícia, devemos chamar a função abrirLink que direciona o usuário para o link da notícia
+                    return (
+                        <TouchableOpacity onPress={() => abrirLink(item.link)}>
+                            <View style={styles.row}>
+                                <Text style={styles.titulo}>{item.title} - {item.pubDate} </Text>
+                            </View>
+                        </TouchableOpacity>
+                    );
+                }}
+            />
+        </>
+    );
+};
+
+//altere os estilos como desejar para melhorar o layout
+const styles = StyleSheet.create({
+    row: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        paddingVertical: 20,
+        paddingHorizontal: 10,
+        borderTopWidth: 1,
+        borderColor: 'gray'
+    },
+    titulo: {
+        fontSize: 14,
+        fontWeight: 'bold'
+    },
+    image: {
+        //pode alterar largura e altura como desejar
+        width: 100,
+        height: 100,
+        borderRadius: 4,
+        margin: 5
+    },
+    descricao: {
+        fontSize: 8
+    },
+    dataPublicacao: {
+        fontSize: 10,
+        fontStyle: 'italic'
+    },
+    icon: {
+        fontSize: 24
+    }
+});
+
+export default ShowFeedScreen;
